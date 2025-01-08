7de50065bda2735cc7c688be73e2ec7d99fc763d
saipepu <saipepu.mdy257@gmail.com>
2025-01-09 02:25:52 +0700

    generate user CRUD resources & setup swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,48 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api')
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+  }));
+
+  const config = new DocumentBuilder()
+  .setTitle('Next Property API')
+  .setVersion('1.0')
+  .addBearerAuth(
+    {
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      name: 'Authorization',
+      description: 'Enter JWT token',
+      in: 'header',
+    },
+    'bearer-token',
+  )
+  .build()
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document,
+    {
+      jsonDocumentUrl: 'swagger-json',
+      swaggerOptions: {
+        operationsSorter: 'method',
+      }
+    }
+  );
+
+  let PORT = 8080
+
+  app.enableCors();
+  await app.listen(PORT, () => console.log("Server is running on port " + PORT));
 }
 bootstrap();
